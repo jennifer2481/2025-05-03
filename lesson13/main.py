d34100942ea1a9024b29b9b88d910e4a367bd86d
jennifer <l0985370525@gmail.com>
2025-06-28 10:09:01 +0800

    modify

---
+++ lesson13/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("我的第一個Streamlit App")
+    st.write("歡迎來到我的應用程式！")
+
+if __name__ == "__main__":
+    main()
+    # import pandas as pd
+    # df = pd.read_csv("taiwan.csv")
+    # st.dataframe(df)
